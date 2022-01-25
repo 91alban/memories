8ac2c2d0ff9101eb36f295aa8d6646ad8aae5d63
91alban <aloku93@gmail.com>
2022-01-25 09:41:15 +0100

    finished on createPost on server side

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,11 +1,12 @@
 import express from 'express';
 
+import { getPosts, createPost } from '../controller/posts.js';
+
 const router = express.Router();
 
 // http://localhost:5000/posts
 
-router.get('/', (req, res) => {
-    res.send('This WORKS!');
-});
+router.get('/', getPosts);
+router.post('/', createPost);
 
 export default router;
